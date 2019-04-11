9b561adff49289ec73fde9eea880c09111422a6b
chen_anguo <cag2050@163.com>
2019-04-11 20:21:34 +0800

    update: README.md

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ Babel、Router(history mode)、Vuex、CSS Pre-processors(Less)、Linter / Format
 
 ### 父组件传递给子组件
 
+方式 | 作用 | 优点 | 缺点 | 适用场景
+--- | --- | --- | --- | ---
+props 方式 | | | |
+slot 方式 | | | |
+refs 方式 | | | `$refs` 只会在组件渲染完成之后生效，并且它们不是响应式的 |
+$children 方式 | | | `$children` 并不保证顺序，开发者也不应该依赖子组件的顺序（ https://cn.vuejs.org/v2/api/#vm-children ）；`vm.$children`，类型：`Array<Vue instance>` | 节制地使用 `$parent` 和 `$children` - 它们的主要目的是作为访问组件的应急方法。更推荐用 v-on_emit 和 events 实现父子组件通信（ https://cn.vuejs.org/v2/api/#parent ）
 
-### 注意事项：
-1. 节制地使用 `$parent` 和 `$children` - 它们的主要目的是作为访问组件的应急方法。更推荐用 v-on_emit 和 events 实现父子组件通信（ https://cn.vuejs.org/v2/api/#parent ）
-2. `$children` 并不保证顺序，开发者也不应该依赖子组件的顺序（ https://cn.vuejs.org/v2/api/#vm-children ）；`vm.$children`，类型：`Array<Vue instance>`
-3. `$refs` 只会在组件渲染完成之后生效，并且它们不是响应式的
-4. 父组件通过 v-on 监听子组件的事件
-5. 子组件修改父组件传来的 prop，然后 emit 一个 update ，父组件监听该事件并根据需要更新一个本地的数据属性。
+### 子组件传递给父组件
+
+方式 | 作用 | 优点 | 缺点 | 适用场景
+--- | --- | --- | --- | ---
+自定义事件（v-on、emit）方式 | 父组件通过 v-on 监听子组件的事件，子组件修改父组件传来的 prop，然后 emit 一个 update ，父组件监听该事件并根据需要更新一个本地的数据属性。| | |
+子组件可以直接修改父组件传来的引用类型 prop | | | |
+.sync 修饰符方式（是自定义事件的语法糖） | | | |
+子组件通过 $parent 设置父组件的值 | | | | 节制使用
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -35,15 +35,16 @@
         <ol>
             <li>
                 <router-link to="/child_to_parent/v-on_emit"
-                    >父组件监听子组件事件 方式：父组件通过 v-on
+                    >自定义事件方式：父组件通过 v-on
                     监听子组件的事件，然后父组件使用子组件触发事件抛出的值（本例中，值是一个1到100之内的随机数）</router-link
                 >
             </li>
             <li>
                 <router-link to="/child_to_parent/child_component_change_prop"
-                    >（不是方式，一个例子）子组件修改接收的基本类型
-                    prop，会报错（如果一定要改，以 update:myPropName
-                    的模式触发事件实现，即下面第3种方法）</router-link
+                    >子组件可以直接修改父组件传来的引用类型
+                    prop：子组件直接修改接收的基本类型
+                    prop，会报错（如果一定要这样做，以 update:myPropName
+                    的模式触发事件实现，即.sync 修饰符方式）</router-link
                 >
             </li>
             <li>
@@ -55,9 +56,17 @@
             </li>
             <li>
                 <router-link to="/child_to_parent/$parent"
-                    >子组件通过 $parent 设置父组件的值（节制使用）</router-link
+                    >$parent 方式：子组件通过 $parent
+                    设置父组件的值（节制使用）</router-link
                 >
             </li>
+        </ol>
+        <h3>非父子组件(兄弟组件、跨级组件)：</h3>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
         </ol>
         <div>===以下是路由的页面内容===</div>
         <router-view />
